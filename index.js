c50cfd7ebea68d0006fab4a164667dc4f096f203
FareedAbbbasi <codingabbas0999@gmail.com>
2025-05-05 19:59:12 +0500

    practice the middelware

--- index.js
+++ index.js
@@ -3,8 +3,47 @@ let express = require("express")
 let app = express();
 app.use(express.json());
 
+let mytoken = 1234
+let mypass = 1234
+
+
+let checkToken = (req, res, next) =>{ 
+    if(req.query.token === "" || req.query.token === undefined) {
+        return res.send({
+            status: 0, 
+            msg: "Please fill the empty token"
+        })
+    }
+
+    if(req.query.token!=mytoken ) {
+        return res.send({
+            status: 0,
+            msg: "Please enter a valid token"
+        })
+    }
+    next();
+}
+
+app.use(checkToken);
+
+app.use((req, res, next) => {
+    if(req.query.pass === "" || req.query.undefined) {
+        return res.send({status: 0, msg: "Please fill the password"})
+    }
+
+    if(req.query.pass != mypass) {
+        return res.send({status: 0, msg: "please enter the valid password"})
+    }
+    next();
+})
+
 app.get("/", (req, res) => {
     res.send({status: 1, msg: "hello I'm Abbasi"})
+})
+
+app.post("/news/:id", (req, res) => {
+    const currentId = req.params.id
+    res.send("The news api " + currentId)
 })
 
 app.get("/contact",(req, res) => {
@@ -13,7 +52,8 @@ app.get("/contact",(req, res) => {
 
 app.post("/login", (req, res) => {
     console.log(req.body)
-    res.send({ status: 1, msg:"Login successfully!", data: req.body})
+    res.status(200).json
+({ status: 1, msg:"Login successfully!", data: req.body})
 })
 
 app.get("/home", (req, res) => {
